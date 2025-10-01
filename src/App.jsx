c60cf996b17f75efd27fb95cd6dc9fd46e1b0384
Daichi Hiraoka <ok230195@oks.iput.ac.jp>
2025-10-01 11:43:20 +0900

    環境構築+Hello World表示まで実装

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Button from './button.jsx';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (  
+    <>
+      <h1>Hello World</h1>
+      <Button type="submit" disabled={true} />
+    </>
+  )
+}
+
+export default App
---
+++ src/Button.jsx
@@ -0,0 +1,13 @@
+function Button(props) {
+
+    const {type, disabled} = props;
+
+    
+  return (
+  <button type={type} disabled={disabled}>
+    クリック
+  </button>
+  
+  );
+}
+export default Button;
