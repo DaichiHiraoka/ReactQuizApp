1ddb468267577df49c6e09154b7924db7d488822
Daichi Hiraoka <ok230195@oks.iput.ac.jp>
2025-10-02 11:15:46 +0900

    紙吹雪の表示範囲を修正

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,17 +1,20 @@
 import styles from './Result.module.css';
 import Confetti from 'react-confetti';
-
+import useWindowSize from 'react-use/lib/useWindowSize'
 
 export default function Result({ maxQuizLen, correctNumLen }) {
+  const { width, height } = useWindowSize();
 
   return (
     <>
-    
     <div className={styles.result}>
       <h1>Result </h1>
       <p>あなたの正解数は: {correctNumLen} / {maxQuizLen}</p>
     </div>
-    <Confetti/>
+    <Confetti
+      width={width}
+      height={height}
+    />
     </>
   );
 }
